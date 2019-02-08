03bef2cdcb7d41ab7509a6bc9347497a0f4e80e6
kokpingli <pingli85@gmail.com>
2019-02-08 17:32:02 +0800

    Remove redundant testing codes

--- src/fractal/util/AreasConsumer.java
+++ src/fractal/util/AreasConsumer.java
@@ -6,12 +6,9 @@ import fractal.model.Area;
 
 public class AreasConsumer implements Runnable {
 	private BlockingQueue<Area> queue;
-	//private BlockingQueue<Integer> queue;
 	private final Area poisonPill;
-	//private final int poisonPill;
 	
 	public AreasConsumer(BlockingQueue<Area> queue, Area poisonPill) {
-	//public AreasConsumer(BlockingQueue<Integer> queue, int poisonPill) {
 		this.queue = queue;
 		this.poisonPill = poisonPill;
 	}
@@ -20,7 +17,6 @@ public class AreasConsumer implements Runnable {
 		try {
 			while (true) {
 				Area area = queue.take();
-				//Integer area = queue.take();
 				if (area.equals(poisonPill)) {
 					return;
 				}
--- src/fractal/util/AreasProducer.java
+++ src/fractal/util/AreasProducer.java
@@ -1,7 +1,6 @@
 package fractal.util;
 
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.ThreadLocalRandom;
 
 import fractal.model.Area;
 import fractal.model.Coordinate;
@@ -10,15 +9,12 @@ import javafx.scene.canvas.GraphicsContext;
 public class AreasProducer implements Runnable {
 
 	private BlockingQueue<Area> areasQueue;
-	//private BlockingQueue<Integer> areasQueue;
 	private final Area poisonPill;
-	//private final int poisonPill;
 	private final int poisonPillPerProducer;
 	private final double height;
 	private final double width;
 	
 	public AreasProducer(GraphicsContext gc, BlockingQueue<Area> areasQueue, Area poisonPill, int poisonPillPerProducer) {
-	//public AreasProducer(GraphicsContext gc, BlockingQueue<Integer> areasQueue, int poisonPill, int poisonPillPerProducer) {
 		this.areasQueue = areasQueue;
 		this.poisonPill = poisonPill;
 		this.poisonPillPerProducer = poisonPillPerProducer;
@@ -37,27 +33,22 @@ public class AreasProducer implements Runnable {
 	private void generateAreas() throws InterruptedException {
 		int areaHeight = 10;
 		int areaWidth = 10;
+		//int areaHeight = 2;
+		//int areaWidth = 2;
 		int id = 0;
 		
-		//for (int i = 0; i < 100; i++) {
 		for (double yCoord = 0.0; yCoord < height; yCoord += areaHeight) {
 			for (double xCoord = 0.0; xCoord < width; xCoord += areaWidth) {
+		//for (double yCoord = 0.0; yCoord < 5; yCoord += areaHeight) {
+		//	for (double xCoord = 0.0; xCoord < 5; xCoord += areaWidth) {
 				Coordinate nextCoord = new Coordinate(xCoord, yCoord);
 				Area area = new Area(id, nextCoord, areaWidth, areaHeight);
-				//areasQueue.put(ThreadLocalRandom.current().nextInt(100));
 				areasQueue.put(area);
 				++id;
 			}
 		}
 		
-/*		for (double yCoord = 0.0; yCoord < height; yCoord += areaHeight) {
-			for (double xCoord = 0.0; xCoord < width; xCoord += areaWidth) {
-				Coordinate nextCoord = new Coordinate(xCoord, yCoord);
-				Area area = new Area(nextCoord, areaWidth, areaHeight);
-				areasQueue.put(area);
-			}
-		}
-*/		for (int j = 0; j < poisonPillPerProducer; ++j) {
+		for (int j = 0; j < poisonPillPerProducer; ++j) {
 			areasQueue.put(poisonPill);
 		}
 		
